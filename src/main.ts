08661f27fd154eadab13ec4e5cc90d552c76d7ed
Christopher Anderson <chrande@microsoft.com>
2017-10-16 19:03:47 -0700

    roll version forward

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { ConfigLoader, IFuncpackConfig } from "./utils";
 
 async function runCli() {
     const p = program
-        .version("0.2.2")
+        .version("0.4.0")
         .option("-d, --debug", "Emits debug messages");
 
     p.command("unpack <path>")
